1d5634ad81b10ebf5ebb92c63aa2c9dc6030de9d
biel678 <bieltomarchio@hotmail.com>
2024-05-10 09:47:16 -0300

    Começando crud de categorias

--- farmacia/src/App.tsx
+++ farmacia/src/App.tsx
@@ -2,6 +2,9 @@ import Home from './pages/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/Categorias/ListaCategoria';
+import FormularioCategoria from './components/Categorias/FormularioCategoria';
+import DeletarCategoria from './components/Categorias/DeletarCategoria';
 
 function App() {
   return ( 
@@ -11,6 +14,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+            <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+            <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
           </Routes>
         </div>
         <Footer />
